4262f75f94b9336633826ffac8c4a88ccb199346
IITG <iitggithub@gmail.com>
2025-01-25 07:35:01 +1100

    updated binary g-code support to make it more readible

--- bricklayers.py
+++ bricklayers.py
@@ -42,31 +42,23 @@ def is_binary_file(input_file, max_bytes=1024):
 
 # Checks if a utility such as bgcode is in the system path or current working directory
 def get_utility_path(utility):
-    try:
-        # Check system path for the utility
-        util_path = shutil.which(utility)
-        if util_path:
-            return util_path
+    # Check system path for the utility
+    util_path = shutil.which(utility)
+    if util_path:
+        return util_path
 
-        # Check current directory for the utility
-        cwd_path = os.path.join(os.getcwd(), utility)
-        logging.info(f"Current Working Directory path: {cwd_path}")
-        if os.path.exists(cwd_path):
-            return cwd_path
+    # Check current directory for the utility
+    cwd_path = os.path.join(os.getcwd(), utility)
+    logging.info(f"Current Working Directory path: {cwd_path}")
+    if os.path.exists(cwd_path):
+        return cwd_path
 
-        # utility not found. Exit the script.
-        raise FileNotFoundError(f"The {utility} utility could not be found. Please make sure it is installed.")
-    except FileNotFoundError as e:
-        error_message = f"An error occurred: {e}"
-        print(error_message, file=sys.stderr)
-        logging.error(error_message)
-        sys.exit(1)
+    # utility was not found on the system
+    return False
 
 # Convert a file from binary G-code format to regular G-code format and vice-versa
-def convert_gcode(input_file):
+def convert_gcode(input_file, bgcode_path):
     try:
-        bgcode_path = get_utility_path("bgcode")
-
         # Convert the input file
         result = subprocess.run(
                                 [bgcode_path, input_file], 
@@ -102,14 +94,31 @@ def process_gcode(input_file, layer_height, extrusion_multiplier):
     # If it's a binary G-code file, convert it back into a regular G-code file for modification
     if is_binary_file(input_file):
         logging.info('Binary G-code detected!')
-        generate_binary_gcode = True
 
+        # Get the path to the bgcode binary
+        bgcode_path = get_utility_path("bgcode")
+        
+        # bgcode not found. Cannot continue.
+        if not bgcode_path:
+            error_message = "The bgcode utility is required in order to modify binary G-code files. Please install it or disable binary G-code generation in your slicer."
+            print(error_message, file=sys.stderr)
+            logging.error(error_message)
+            sys.exit(1)
+
+        # File extension needs to be .bgcode for bgcode
+        # to convert it correctly.
         if not input_file.endswith('.bgcode'):
             binary_input_file = f"{input_file}.bgcode"
             os.rename(input_file, binary_input_file)
-            convert_gcode(binary_input_file)
-            os.remove(binary_input_file)
+
+            # Convert the binary G-code into regular G-code
+            convert_gcode(binary_input_file, bgcode_path)
+
+            # Rename the converted gcode file back to its original name
             os.rename(f"{input_file}.gcode", input_file)
+
+        # Inform the script to perform G-code to binary G-code conversion at the end
+        generate_binary_gcode = True
 
     # Read the input G-code.
     with open(input_file, 'r') as infile:
@@ -199,10 +208,10 @@ def process_gcode(input_file, layer_height, extrusion_multiplier):
     if generate_binary_gcode:
         logging.info("Converting G-code to back to binary G-code")
         if not input_file.endswith('.gcode'):
-            binary_outfile = f"{input_file}.gcode"
-            os.rename(input_file, binary_outfile)
-            convert_gcode(binary_outfile)
-            os.remove(binary_outfile)
+            tempfile = f"{input_file}.gcode"
+            os.rename(input_file, tempfile)
+            convert_gcode(tempfile, bgcode_path)
+            os.remove(tempfile)
             os.rename(f"{input_file}.bgcode", input_file)
 
     logging.info("G-code processing completed")
