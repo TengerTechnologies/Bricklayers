c9984803280f1dc68d6ccce98327ca68b2fd0657
Mubeen Khan <8978476+mubeenkhan94@users.noreply.github.com>
2025-02-01 01:16:03 -0600

    actually fixed header

--- bricklayers.py
+++ bricklayers.py
@@ -9,7 +9,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program. If not, see .
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 #
 # Copyright (c) [2025] [Roman Tenger]
 
